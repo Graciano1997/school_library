0f12d16a34d65b5d6bb59cfbeb6b62137c8ccad6
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-10-11 15:11:56 +0100

    fix linter issues

--- person.rb
+++ person.rb
@@ -1,26 +1,24 @@
 class Person
-
-  def initialize(name="Unknown",age,parent_permission=true)
-    @id=Math.sin(rand(0..45)*Math::PI) + Math.acos(rand(45..360)*Math::PI) + 1
-    @name=name
-    @age=age
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(1..1001)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
+  attr_reader :id
+  attr_accessor :name, :age
 
-  attr_reader:id
-  attr_accessor:name
-  attr_accessor:age
-
-  def of_age?
-    if @age >= 18
+  def can_use_services?
+    if of_age? || @parent_permission
       true
     else
       false
     end
   end
 
-  def can_use_services?
-    if of_age?() || parent_permission
-      true
-    else
-      false
+  private
+
+  def of_age?
+    @age >= 18
+  end
 end
--- student.rb
+++ student.rb
@@ -7,6 +7,6 @@ class Student < Person
   end
 
   def play_hooky
-    puts %{¯(ツ)/¯}
+    puts '¯(ツ)/¯'
   end
 end
