6dac0641faa1f67c3448561fe0cafa8553a46c11
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-10-13 09:57:46 +0100

    update list all books and person methods

--- app.rb
+++ app.rb
@@ -1,19 +1,79 @@
+require_relative "student"
+require_relative "teacher"
+class App
+  @people=[]
+  @books=[]
+
 def list_all_book
+  @books.each do |book|
+    puts "Title: #{book.title}, Author: #{book.author}"
+  end
 end
 
 def list_all_people
+  @people.each do |person,i|
+    category= person.specialization? "Teacher" : "Student"
+    puts "#{i}) [#{category}] Name: #{person.name}, ID: #{person.ID} Age: #{person.age}"
+  end
 end
 
-def list_all_rentals(id)
+def list_all_rentals(person_id)
 end
 
-def create_person(teacher)
+def create_person
+  puts "Do you want to create a Teacher(1) or a Student(2) ?"
+  person=gets.chomp
+  case person
+  when 1
+    puts "Name :"
+    name=gets.chomp
+    name=="Unknown" unless name!=""
+    puts "Age :"
+    age=gets.chomp
+    puts "Specialization :"
+    specialization=gets.chomp
+    parent_permission=true
+    teacher=Teacher.new(age, specialization, name,parent_permission)
+    people << teacher
+    puts "Person Created Successfully"
+  when 2
+    puts "Name :"
+    name=gets.chomp
+    name=="Unknown" unless name!=""
+    puts "Age :"
+    age=gets.chomp
+    puts "ClassRoom :"
+    classroom=gets.chomp
+    puts "Permission (Y/N) :"
+    permission=gets.chomp
+    case permission
+      when "N" || "n"
+    parent_permission=false
+      when "Y" || "y"
+    parent_permission=true
+    puts "Person Created Successfully"
+      else
+        puts "Please Ensure to type (Y or N) || (y or n)"
+      end
+    student=Student.new(age, classroom, name, parent_permission)
+    @people << student
+  else
+    puts "Please Enrure to choose 1 to Create a Teacher or 2 to a student"
+  end
 end
 
 def create_rental
+
 end
 
-def create__book
+def create_book
+  puts "Title :"
+  title=gets.chomp
+  puts "Author :"
+  author=gets.chomp
+  book=Book.new(title, author)
+  @books << book
+  puts "Book Created Successfully"
 end
 
-Create a person (teacher or student, not a plain Person).
+end
--- main.rb
+++ main.rb
@@ -2,9 +2,7 @@ require_relative 'person'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+Present the user with a list of options to perform.
+Lets users choose an option.
+If needed, ask for parameters for the option.
+Have a way to quit the app.
