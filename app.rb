a6052cb9261a3b257c383c23a38bed1ba4cd0ce5
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-10-17 23:20:14 +0100

    add required changes

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require_relative 'student'
 require_relative 'teacher'
 require_relative 'rental'
 require_relative 'book'
+require_relative 'validateperson'
+
 
 class App
   def initialize
@@ -61,36 +63,12 @@ class App
     puts ' '
     puts '_________________CREATING A PERSON 👨👨 _________________________'
     puts 'Do you want to create a Teacher(1️⃣  ) or a Student(2️⃣  ) ?'
-    person = gets.chomp.to_i
-    if person == 1
-      puts 'Name :'
-      name = gets.chomp
-      puts 'Age :'
-      age = gets.chomp
-      puts 'Specialization :'
-      specialization = gets.chomp
-      teacher = Teacher.new(age, specialization, name)
-      @people << teacher
-      puts 'Person Created Successfully 👨🤩'
-      puts '__________________________________________'
-    elsif person == 2
-      puts 'Name :'
-      name = gets.chomp
-      puts 'Age :'
-      age = gets.chomp
-      puts 'ClassRoom :'
-      classroom = gets.chomp
-      puts 'Permission (Y/N) :'
-      permission = gets.chomp.capitalize
-      parent_permission = permission == 'Y'
-      student = Student.new(age, classroom, parent_permission, name)
-      @people << student
-      puts 'Person Created Successfully 👨🤩'
-      puts '__________________________________________'
-    else
-      puts 'Please Enrure to choose Teacher(1️⃣  ) or a Student(2️⃣  )'
+    person_option = gets.chomp.to_i
+    factory_person=ValidatePerson.new(person_option)
+    @people << factory_person.person
+    puts 'Person Created Successfully 👨🤩'
+    puts '__________________________________________'
     end
-  end
 
   def create_book
     puts ' '
--- validateperson.rb
+++ validateperson.rb
@@ -2,34 +2,33 @@ require_relative 'student'
 require_relative 'teacher'
 
 class ValidatePerson
+  attr_reader :person
   def initialize(option)
     puts 'Name :'
     @name = gets.chomp
     puts 'Age :'
     @age = gets.chomp
-
     if option.to_i == 1
       @person = create_teacher
-      @person
     end
-
     return unless option.to_i == 2
-
     @person = create_student
   end
 
   def create_teacher
     puts 'Specialization :'
-    specialization = gets.chomp
-    Teacher.new(@age, specialization, @name)
+    @specialization = gets.chomp
+    teacher=Teacher.new(@age, @specialization, @name)
+    teacher
   end
 
   def create_student
     puts 'ClassRoom :'
-    classroom = gets.chomp
+    @classroom = gets.chomp
     puts 'Permission (Y/N) :'
     permission = gets.chomp.capitalize
-    parent_permission = permission
-    Student.new(@age, classroom, parent_permission, @name)
+    @parent_permission = permission
+    student=Student.new(@age, @classroom, @parent_permission, @name)
+    student
   end
 end
