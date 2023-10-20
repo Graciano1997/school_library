b9fa23bf1afe7cf0a6491e501f450a91f54bd2cd
wac <wabaham9@gmail.com>
2023-10-20 13:31:30 +0300

    Add tests for base decorator, capitalize decorator and book

--- spec/spec_decorator.rb
+++
@@ -1,9 +0,0 @@
-require_relative ('decorator')
-describe BaseDecorator do
-    context 'base decorator class' do
-        it '(classroom) should be an instance of Classroom class' do
-            classroom = BaseDecorator.new('Room 2')
-            expect(classroom).to be_instance_of BaseDecorator
-        end
-    end
-end
